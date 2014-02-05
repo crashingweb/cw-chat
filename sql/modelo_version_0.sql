93827bdd22e6198a14628f7f7a808e4614913358
Kevin Alvarez <kevalvmou@gmail.com>
2014-02-04 22:55:35 -0500

    Modelo
    SQL

--- sql/modelo_version_0.sql
+++ sql/modelo_version_0.sql
@@ -61,5 +61,5 @@ CREATE TABLE IF NOT EXISTS mensaje_usuario_conversacion (
 
 #Tablas version 1#
 
-INSERT INTO Conversacion VALUES (1,now(),'1');
+INSERT INTO conversacion VALUES (1,now(),'1');
 INSERT INTO `usuario_conversacion` (`id_usuario_conversacion`, `id_usuario`, `id_conversacion`, `estado`) VALUES ('1', '1', '1', '1');
